1ff7c227b5b911fc1533dc55a085122416c7cb4c
Shaneil Clarke <shaneilske@yahoo.com>
2019-04-01 10:32:26 +0100

    added page title component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import PageTitle from './components/headers/PageTitle'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+       <header>
+         <PageTitle />
+       </header>
       </div>
     );
   }
---
+++ src/components/headers/PageTitle.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PageTitle extends Component {
+  render() {
+    return <h1>Game of Thrones API</h1>;
+  }
+}
+
+export default PageTitle;
